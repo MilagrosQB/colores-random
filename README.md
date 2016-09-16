c0823ed3ddd58d2e1730cfe92cb2595ddd3835d5
MilagrosQB <qbmilagros@gmail.com>
2016-09-16 09:41:27 -0500

    cri agregado, readme tmb

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 **Colores Random**
 ![Referencia HTML](http://i63.tinypic.com/iekv3n.jpg)
 
+Cri
+*La función del botón Cri es cambiar el color del borde al hacerle clic. *
+![JS Cri](http://i65.tinypic.com/2uts0v4.jpg)
+
 SuperCri
 *La función del botón SuperCri es cambiar el color cuando se digite el nombre del color en el recuadro Color. Los únicos colores que podrán aparecer son Naranja, Verde y Morado*
 
 ![JS SuperCri](http://i65.tinypic.com/zlvp0m.jpg)
+
+
+Ambos botones generan un evento
--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 //Cri
 window.addEventListener("load", function() {
 	var boton = document.getElementById("btn-cri");
+	var contador = 1;
+	
+	boton.addEventListener("click", function() {
+		if(contador == 1) {
+			document.getElementById("color").value = "red";
+			document.getElementById("orange").classList.add("border-green");
+			document.getElementById("green").classList.remove("border-orange");
+		}
+		if(contador == 2) {
+			document.getElementById("color").value = "orange";
+			document.getElementById("green").classList.add("border-purple");
+			document.getElementById("purple").classList.remove("border-green");
+		}
+		if(contador == 3) {
+			document.getElementById("color").value = "green";
+			document.getElementById("purple").classList.add("border-orange");
+			document.getElementById("orange").classList.remove("border-purple");
+			contador = 0;
+		}
+		contador++;
+	});	
 
 });
 
